88bb57ed44f3fc1caf7e3fae5f1f126ad04e7340
Leo <leeyuhinleo@gmail.com>
2018-03-26 14:50:33 +0800

    ADD: solution of 7. Reverse Integer

--- 7.rb
+++ 7.rb
@@ -1,6 +1,12 @@
 # 7. Reverse Integer
+# 1032 / 1032 test cases passed.
+# Status: Accepted
+# Runtime: 52 ms
 # @param {Integer} x
 # @return {Integer}
 def reverse(x)
-    
+    r = x.to_s.split('').reverse.join('').to_i
+    r *= -1 if x < 0
+    r = 0 if r > 2147483647 || r < -2147483648
+    return r
 end
