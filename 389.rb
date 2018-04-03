b16bc6258616106047077337d6f460c6b59d665b
Leo <leeyuhinleo@gmail.com>
2018-04-03 23:08:53 +0800

    ALT: improve solution of 389. Find the Difference

--- 389.rb
+++ 389.rb
@@ -1,11 +1,20 @@
 # 389. Find the Difference
 # 54 / 54 test cases passed.
 # Status: Accepted
-# Runtime: 100 ms
+# Runtime: 52 ms
 # @param {String} s
 # @param {String} t
 # @return {Character}
 def find_the_difference(s, t)
     return t if s.empty?
-    (t.split('').map(&:ord).reduce(&:+) - s.split('').map(&:ord).reduce(&:+)).chr
+    count = Hash.new(0)
+    s.each_char do |c|
+      count[c] += 1
+    end
+    t.each_char do |c|
+      count[c] -= 1
+    end
+    count.detect {|k,v| v < 0}[0]
 end
+
+puts find_the_difference('aa', 'aab')
