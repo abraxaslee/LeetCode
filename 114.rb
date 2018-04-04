420273ac96de0e6ac6394a019d9b44ef6215121d
Leo <leeyuhinleo@gmail.com>
2018-04-05 00:03:41 +0800

    ALT: improve solution of 114. Flatten Binary Tree to Linked List

--- 114.rb
+++ 114.rb
@@ -1,7 +1,7 @@
 # 114. Flatten Binary Tree to Linked List
 # 225 / 225 test cases passed.
 # Status: Accepted
-# Runtime: 72 ms
+# Runtime: 44 ms
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -11,19 +11,21 @@
 #     end
 # end
 
-def traverse(node)
-    @tree.push node
-    traverse(node.left) unless node.left == nil
-    traverse(node.right) unless node.right == nil
-end
-
 # @param {TreeNode} root
 # @return {Void} Do not return anything, modify root in-place instead.
 def flatten(root)
-    @tree = []
-    traverse(root) unless root == nil
-    @tree.each_with_index do |node, i|
-        node.left = nil
-        node.right = i < @tree.length ? @tree[i+1] : nil
+    return if root == nil
+    if root.left
+        if root.right
+            node = root.left
+            while node.right || node.left
+              node = node.right ? node.right : node.left
+            end
+
+            node.right, root.right, root.left = root.right, root.left, nil
+        else
+            root.right, root.left = root.left, nil
+        end
     end
+    flatten(root.right)
 end
