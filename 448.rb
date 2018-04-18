3885a7aaf9f859ad69e1b8d1985cc11fd72c77aa
Leo <leeyuhinleo@gmail.com>
2018-04-18 22:23:02 +0800

    ALT: improve solution of 448. Find All Numbers Disappeared in an Array

--- 448.rb
+++ 448.rb
@@ -1,10 +1,10 @@
 # 448. Find All Numbers Disappeared in an Array
 # 34 / 34 test cases passed.
 # Status: Accepted
-# Runtime: 306 ms
+# Runtime: 124 ms
 
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_disappeared_numbers(nums)
-    ((1..nums.length).to_a - nums).sort!
+    (1..nums.length).to_a - nums
 end
