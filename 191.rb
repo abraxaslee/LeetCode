42b63f99d07dd208c70664ce55ad2f106fa07a46
Leo <leeyuhinleo@gmail.com>
2018-04-21 00:52:54 +0800

    ALT: improve solution of 191. Number of 1 Bits

--- 191.rb
+++ 191.rb
@@ -1,13 +1,10 @@
 # 191. Number of 1 Bits
-# 
 # 600 / 600 test cases passed.
 # Status: Accepted
-# Runtime: 82 ms
+# Runtime: 36 ms
 
 # @param {Integer} n, a positive integer
 # @return {Integer}
 def hamming_weight(n)
-    result = n.to_s(2).split('').select {|x| x == '1'}.map(&:to_i).reduce(&:+)
-    return 0 if result.nil?
-    return result
+    n.to_s(2).count('1')
 end
